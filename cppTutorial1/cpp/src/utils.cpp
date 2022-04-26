0aabf3c075a74913171c01a1348252730401bb53
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-26 06:48:00 +0100

    Adding a final set of basics with C++ & a cleaner for executables before commit. :)

---
+++ cppTutorial1/cpp/src/utils.cpp
@@ -0,0 +1,7 @@
+#include "../include/cppTutorial1_bits/utils.hpp"
+#include <iostream>
+
+void doSomething()
+{
+    std::cout << "Doing something here ..." << std::endl;
+}
---
+++ cppTutorial1/cpp/tests/src/headersInclude.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cppTutorial1>
+
+using namespace std;
+
+int main()
+{
+    doSomething();
+    doSomething();
+
+    return 0;
+}
