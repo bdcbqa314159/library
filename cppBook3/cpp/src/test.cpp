c61e0a547ed2971d3a68882bfa29c3fce317ce2c
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-06-04 21:41:35 +0100

    Starting a new book about Data Structures & Algorithms in c++.:)

---
+++ cppBook3/cpp/src/test.cpp
@@ -0,0 +1,7 @@
+#include "../include/cppBook3_bits/test.hpp"
+#include <iostream>
+
+void message()
+{
+    std::cout << "Another Book about DSA yay! ->testing everything is OK!" << std::endl;
+}
---
+++ cppBook3/cpp/tests/src/test.cpp
@@ -0,0 +1,17 @@
+#include <cppBook3>
+#include <iostream>
+
+using namespace std;
+
+void test()
+{
+    message();
+}
+
+int main()
+{
+
+    test();
+
+    return 0;
+}
